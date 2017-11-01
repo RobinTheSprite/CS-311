d28be5447c37de185229f7cd8f9b410219142239
RobinTheSprite <madscientistak@gmail.com>
2017-10-31 19:44:27 -0800

    Assn. 5 complete

--- tssarray.h
+++ tssarray.h
@@ -4,16 +4,23 @@ Mark Underwood
 10.19.17
 Contains declarations and definitions for TSSArray class
 */
-#pragma warning (disable : 4996)
 #ifndef FILE_TSSARRAY_H_INCLUDED
 #define FILE_TSSARRAY_H_INCLUDED
 #include <algorithm>
 
-
+//TSSArray
+//"Smart array" class that knows its size and can have elements added or removed.
+//Requirements on Types:
+//	-All elements in the array are of type dataType.
+//Class Invariants:
+//	-The class object owns a pointer of type dataType*, which points to a dynamically allocated array of type dataType.
+//	-The size of the array is recorded in a size_type variable _size.
+//	-The capacity of the array is >= 16 and is recorded in a size_type variable _capacity.
+//	-Any object declared as "const TSSArray" is not modified in any way.
 template <typename dataType>
 class TSSArray
 {
-public:
+public:	//Member Types and Constants
 	using size_type = size_t; 
 	using value_type = dataType;
 	using iterator = value_type *;
@@ -26,6 +33,7 @@ public:
 
 	//Default Ctor and One-Param Ctor
 	//Strong Guarantee
+	//Exception-neutral
 	explicit TSSArray(size_type size = 0)
 		: _capacity(std::max((size * 2), minimumCapacity)),
 		_size(size),
@@ -33,7 +41,8 @@ public:
 	{}
 
 	//Dtor
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	~TSSArray()
 	{
 		delete [] _data;
@@ -41,6 +50,7 @@ public:
 
 	//Copy Constructor
 	//Strong Guarantee
+	//Exception-neutral
 	TSSArray(const TSSArray & toBeCopied)
 		: _capacity(toBeCopied._capacity),
 		  _size(toBeCopied._size),
@@ -58,62 +68,71 @@ public:
 	}
 
 	//Move Constructor
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	TSSArray(TSSArray && toBeCopied) noexcept
 	{
 		swap(toBeCopied);
 		toBeCopied._data = nullptr;
 		toBeCopied._size = 0;
+		toBeCopied._capacity = 0;
 	}
 
 	/**********Misc. "Get" Functions***********/
 
 	//size
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	size_type size() const noexcept
 	{
 		return _size;
 	}
 
 	//begin (non-const)
-	//No-Throw 
+	//No-Throw Guarantee
+	//Exception-neutral
 	value_type * begin() noexcept
 	{
 		return _data;
 	}
 
 	//begin (const)
-	//No-Throw 
+	//No-Throw Guarantee
+	//Exception-neutral
 	const value_type * begin() const noexcept
 	{
 		return _data;
 	}
 
 	//end (non-const)
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	value_type * end() noexcept
 	{
 		return (begin() + _size);
 	}
 
 	//end (const)
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	const value_type * end() const noexcept
 	{
 		return (begin() + _size);
 	}
 
 	//empty
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	bool empty() const noexcept
 	{
-		return _size == size_type(0);
+		return _size == 0;
 	}
 
 	/**********Modifying Functions***********/
 
 	//swap
-	//No-Throw
+	//No-Throw Guarantee
+	//Exception-neutral
 	void swap(TSSArray & other) noexcept
 	{
 		std::swap(_data, other._data);
@@ -122,24 +141,23 @@ public:
 	}
 
 	//resize
-
+	//Strong Guarantee
+	//Exception-neutral
 	void resize(size_type newsize)
 	{
 		if (newsize > _capacity)
 		{
-			size_type newCapacity;
-			value_type *  newData;
+			TSSArray newArray(newsize);
+			try
+			{
+				std::copy(begin(), end(), newArray.begin());
+			}
+			catch (...)
+			{
+				throw;
+			}
 
-			newCapacity = size_type(newsize * 2);
-			newData = new value_type[newCapacity];
-
-			std::copy(begin(), end(), newData);
-			delete[] _data;
-
-			_capacity = newCapacity;
-			_size = newsize;
-			_data = newData;
-			newData = nullptr;
+			*this = newArray;
 		}
 		else
 		{
@@ -148,39 +166,49 @@ public:
 	}
 
 	//insert
-
-	iterator insert(iterator it, value_type item)
+	//Pre: insertLoc >= begin() and <= end()
+	//Strong Guarantee
+	//Exception-neutral
+	iterator insert(iterator insertLoc, value_type item)
 	{
+		size_type distance = end() - insertLoc;
 		resize(_size + 1);
-		for (iterator i = end(); i > it; --i)
+		insertLoc = end() - distance - 1; 
+
+		for (iterator i = (end()-1); i > insertLoc; --i)
 		{
 			*i = *(i - 1);
 		}
-		*it = item;
-		return it;
+		*insertLoc = item;
+
+		return insertLoc;
 	}
 
 	//erase
-
-	iterator erase(iterator it)
+	//Pre: eraseLoc >= begin() and < end()
+	//Strong Guarantee
+	//Exception-neutral
+	iterator erase(iterator eraseLoc)
 	{
-		for (iterator i = it; i < end(); ++i)
+		for (iterator i = eraseLoc; i < end(); ++i)
 		{
 			*i = *(i + 1);
 		}
 		resize(_size - 1);
-		return it;
+		return eraseLoc;
 	}
 
 	//push_back
-
+	//Strong Guarantee
+	//Exception-neutral
 	void push_back(value_type item)
 	{
 		insert(end(), item);
 	}
 
 	//pop_back
-
+	//Strong Guarantee
+	//Exception-neutral
 	void pop_back()
 	{
 		erase(end() - 1);
@@ -189,7 +217,8 @@ public:
 	/**********Member Operators***********/
 
 	//op= (Copy)
-	//Pre: None
+	//Strong Guarantee
+	//Exception-neutral
 	TSSArray & operator=(const TSSArray & lhs)
 	{
 		TSSArray tempLhs(lhs);
@@ -199,6 +228,8 @@ public:
 
 	//op= (Move)
 	//Pre: rValueTrash must be an rvalue.
+	//No-Throw Guarantee
+	//Exception-neutral
 	TSSArray & operator=(TSSArray && rValueTrash) noexcept
 	{
 		swap(rValueTrash);
@@ -206,15 +237,19 @@ public:
 	}
 
 	//op[] (non-const)
-	//Pre: index >= 0
-	value_type & operator[](size_type index)
+	//Pre: index >= 0 and < size
+	//No-Throw Guarantee
+	//Exception-neutral
+	value_type & operator[](size_type index) noexcept
 	{
 		return _data[index];
 	}
 
 	//op[] (const)
-	//Pre: index >= 0
-	const value_type & operator[](size_type index) const
+	//Pre: index >= 0 and < size
+	//No-Throw Guarantee
+	//Exception-neutral
+	const value_type & operator[](size_type index) const noexcept
 	{
 		return _data[index];
 	}
